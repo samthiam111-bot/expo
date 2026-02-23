6f0e429c3e750a94898d4672c6ccf865edca481b
Aman Mittal <amandeepmittal@live.com>
2026-02-24 01:10:17 +0530

    Update index.tsx

--- docs/ui/components/StructuredData/index.tsx
+++ docs/ui/components/StructuredData/index.tsx
@@ -1,14 +1,15 @@
 import Head from 'next/head';
 
 type Props = {
+  id: string;
   data: Record<string, any>;
 };
 
-export function StructuredData({ data }: Props) {
+export function StructuredData({ id, data }: Props) {
   return (
     <Head>
       <script
-        key="structured-data"
+        key={`structured-data-${id}`}
         type="application/ld+json"
         dangerouslySetInnerHTML={{ __html: JSON.stringify(data) }}
       />
